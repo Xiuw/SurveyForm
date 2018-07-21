5324d574032317a4e3236d97199aae5e1b9bc16a
xiuw <sylvi.xw@gmail.com>
2018-07-21 14:34:06 -0400

    Update

---
+++ script.js
@@ -0,0 +1,76 @@
+/*********************
+
+Validation for form 
+
+
+************************/
+
+//Validation error when email is not enter in correct format
+
+
+
+//Validation error when non-numbers is enter for age
+// Validation error when number is outside of the range of age input
+
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(document.addEventListener('DOMContentLoaded', function(e) {
+    FormValidation.formValidation(
+        document.getElementById('demoForm'),
+        {
+            fields: {
+                name: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The name is required'
+                        },
+                        stringLength: {
+                            min: 6,
+                            max: 30,
+                            message: 'The name must be more than 6 and less than 30 characters long'
+                        },
+                        regexp: {
+                            regexp: /^[a-zA-Z0-9_]+$/,
+                            message: 'The name can only consist of alphabetical, number and underscore'
+                        }
+                    }
+                },
+                price: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The price is required'
+                        },
+                        numeric: {
+                            message: 'The price must be a number'
+                        }
+                    }
+                },
+                'size[]': {
+                    validators: {
+                        notEmpty: {
+                            message: 'The size is required'
+                        }
+                    }
+                },
+                availability: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The availability option is required'
+                        }
+                    }
+                },
+            },
+            plugins: {
+                trigger: new FormValidation.plugins.Trigger(),
+                bootstrap: new FormValidation.plugins.Bootstrap(),
+                submitButton: new FormValidation.plugins.SubmitButton(),
+                icon: new FormValidation.plugins.Icon({
+                    valid: 'fa fa-check',
+                    invalid: 'fa fa-times',
+                    validating: 'fa fa-refresh'
+                }),
+            },
+        }
+    );
+});
